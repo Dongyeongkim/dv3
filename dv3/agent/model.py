fbf6499783cefc34db7f46851cca0212c89de9c2
dongyeongkim <dongyeongkim33@gmail.com>
2023-06-05 17:56:19 +0900

    dv3 update

---
+++ dv3/agent/model.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+
+
+class RSSM(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        pass
+
+
+class Encoder(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        pass
+
+class Decoder(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        pass
+
+
+class DynamicsPredictor(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        pass
+
+
+class RewardPredictor(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        pass
+
+class ContinuePredictor(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        pass
+
+
+
