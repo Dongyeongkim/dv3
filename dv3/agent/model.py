019603f349807bef2b1ce7fabe9546c137ff8c4e
dongyeongkim <dongyeongkim33@gmail.com>
2023-06-06 19:54:56 +0900

    almost all updates and check the right shape of encoder and decoder

--- dv3/agent/model.py
+++ dv3/agent/model.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 
@@ -11,7 +12,16 @@ class Addition(nn.Module):
 
 # Return proper activation functions 
 
-def return_func_from_name(name: str or int or float, ch: int, it: int):
+# should fix this function to get nice and proper output
+
+def conv_shape_calc(input_size: int, kernel_size: int, stride_size: int, padding_size: int):
+    return math.floor((input_size - kernel_size + 2*padding_size)/stride_size) + 1
+
+def deconv_shape_calc(input_size: int, kernel_size: int, stride_size: int, padding_size: int, output_padding_size: int):
+    return stride_size*(input_size - 1) + kernel_size - 2*padding_size + output_padding_size
+
+
+def return_func_from_name(name: str or int or float, ch: int, sh: int):
 
     if name == 'relu':
         return [nn.ReLU()]
@@ -20,7 +30,7 @@ def return_func_from_name(name: str or int or float, ch: int, it: int):
         return [nn.Sigmoid()]
     
     elif name == 'layernorm+silu':
-        return [nn.LayerNorm([ch, 2**(5-it), 2**(5-it)]), nn.SiLU()]
+        return [nn.LayerNorm([ch, sh, sh]), nn.SiLU()]
     
     elif ((type(name) == int) or (type(name) == float)):
         return [Addition(name)]
@@ -47,17 +57,21 @@ class GRU_ln_Cell(nn.Module):
 
 
 class Encoder(nn.Module):
-    def __init__(self, in_channel: int, channels: list, kernels: list, strides: list, paddings: list, activations: list):
+    def __init__(self, input_size: int, in_channel: int, channels: list, kernels: list, 
+                 strides: list, paddings: list, activations: list):
         super().__init__()
         enc = []
+        shp = input_size
         enc.append(nn.Conv2d(in_channels=in_channel, out_channels=channels[0], 
                              kernel_size=kernels[0], stride=strides[0], padding=paddings[0]))
-        enc.extend(return_func_from_name(activations[0], ch=channels[0], it=0)) 
+        shp = conv_shape_calc(shp, kernel_size=kernels[0], stride_size=strides[0], padding_size=paddings[0])
+        enc.extend(return_func_from_name(activations[0], ch=channels[0], sh=shp)) 
 
         for i in range(len(channels)-1):
             enc.append(nn.Conv2d(in_channels=channels[i], out_channels=channels[i+1],
                                  kernel_size=kernels[i+1], stride=strides[i+1], padding=paddings[i+1]))
-            enc.extend(return_func_from_name(activations[i+1], ch=channels[i+1], it=i+1))
+            shp = conv_shape_calc(shp, kernel_size=kernels[i+1], stride_size=strides[i+1], padding_size=paddings[i+1])
+            enc.extend(return_func_from_name(activations[i+1], ch=channels[i+1], sh=shp))
         
         self.enc = nn.Sequential(*enc)
     def forward(self, x):
@@ -65,17 +79,25 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, out_channel: int, channels: list, kernels: list, strides: list, paddings: list, activations: list):
+    def __init__(self, input_size: int, out_channel: int, channels: list, kernels: list,
+                  strides: list, paddings: list, activations: list):
         super().__init__()
         dec = []
+        shp = input_size
         for i in range(len(channels)-1):
-            dec.append(nn.Conv2d(in_channels=channels[i], out_channels=channels[i+1],
-                                 kernel_size=kernels[i], stride=strides[i], padding=paddings[i]))
-            dec.extend(return_func_from_name(activations[i],ch=channels[i], it=i))
+            dec.append(nn.ConvTranspose2d(in_channels=channels[i], out_channels=channels[i+1],
+                                 kernel_size=kernels[i], stride=strides[i], padding=paddings[i], output_padding=paddings[i]))
+            shp = deconv_shape_calc(shp, kernel_size=kernels[i], stride_size=strides[i], padding_size=paddings[i],
+                                    output_padding_size=paddings[i])
+            dec.extend(return_func_from_name(activations[i],ch=channels[i+1], sh=shp))
         
-        dec.append(nn.Conv2d(in_channels=channels[-1], out_channels=out_channel,
-                             kernel_size=kernels[-1], stride=strides[-1], padding=paddings[-1]))
-        dec.extend(return_func_from_name(activations[-1], ch=channels[-1], it=-1))
+        dec.append(nn.ConvTranspose2d(in_channels=channels[-1], out_channels=out_channel,
+                             kernel_size=kernels[-1], stride=strides[-1], padding=paddings[-1], output_padding=paddings[-1]))
+        shp = deconv_shape_calc(shp, kernel_size=kernels[-1], stride_size=strides[-1], padding_size=paddings[-1],
+                                 output_padding_size=paddings[-1])
+        print(shp)
+
+        dec.extend(return_func_from_name(activations[-1], ch=channels[-1], sh=shp))
 
         self.dec = nn.Sequential(*dec)
 
@@ -138,7 +160,7 @@ if __name__ == "__main__":
     
     dv1_enc_conf = conf['model']['enc']['dv1']
     dv2_enc_conf = conf['model']['enc']['dv2']
-    dv3_enc_conf = conf['model']['enc']['dv3']['xl']
+    dv3_enc_conf = conf['model']['enc']['dv3']['m']
 
     dv1Enc = Encoder(*dv1_enc_conf.values())
     dv2Enc = Encoder(*dv2_enc_conf.values())
@@ -150,7 +172,7 @@ if __name__ == "__main__":
 
     dv1_dec_conf = conf['model']['dec']['dv1']
     dv2_dec_conf = conf['model']['dec']['dv2']
-    dv3_dec_conf = conf['model']['dec']['dv3']['xl']
+    dv3_dec_conf = conf['model']['dec']['dv3']['m']
 
     dv1Dec = Decoder(*dv1_dec_conf.values())
     dv2Dec = Decoder(*dv2_dec_conf.values())
@@ -158,6 +180,20 @@ if __name__ == "__main__":
 
     print(dv1Dec, dv2Dec, dv3Dec)
 
-    test_input = torch.randn(32, 1, 64, 64)
-    dv1_er, dv2_er, dv3_er = dv1Enc(test_input), dv2Enc(test_input), dv3Enc(test_input)
+    test_enc_input = torch.randn(32, 1, 64, 64)
+    dv1_er = dv1Enc(test_enc_input)
+    dv2_er = dv2Enc(test_enc_input)
+    dv3_er = dv3Enc(test_enc_input)
+    
     print(dv1_er.shape, dv2_er.shape, dv3_er.shape)
+    
+    test_dec_input_dv1 = torch.randn(32, 1024, 1, 1)
+    test_dec_input_dv2 = torch.randn(32, 1024, 1, 1)
+    test_dec_input_dv3 = torch.randn(32, 384,  4, 4)
+    dv1_re = dv1Dec(test_dec_input_dv1)
+    print('pass')
+    dv2_re = dv2Dec(test_dec_input_dv2)
+    print('pass')
+    dv3_re = dv3Dec(test_dec_input_dv3)
+    print('pass')
+    print(dv1_re.shape, dv2_re.shape, dv3_re.shape)
